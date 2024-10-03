033e160dd948965dcc609e00b0f7fe6d4901c6d0
pragnakalpdev47 <pragnakalp.dev47@gmail.com>
2024-10-03 18:47:16 +0530

    add

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,9 +1,9 @@
 import pytest
 # from app import app
 
-import sys
-import os
-sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
+# import sys
+# import os
+# sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), '..')))
 from app import app
 
 
